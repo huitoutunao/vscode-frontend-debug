c227656d1ee6e31c923af3583fda02e5f8aeea4c
huitoutunao <lh_huitoutunao@163.com>
2024-09-14 23:25:46 +0800

    添加了click-to-react-component作为dev依赖，并在应用的入口文件中引入了ClickToComponent组件。此外，对package-lock.json文件进行了更新，记录了新增依赖及其相关信息。

--- test-react-debug/src/index.js
+++ test-react-debug/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ClickToComponent } from 'click-to-react-component';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ClickToComponent />
     <App />
   </React.StrictMode>
 );
