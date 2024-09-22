01b004e6e3d6ebdf38b3f4cc8bfaa75e9841c363
huitoutunao <lh_huitoutunao@163.com>
2024-09-22 22:03:43 +0800

    feat: 新增eslint调试50%

---
+++ eslint-debug/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    root: true,
+    extends: ['standard']
+    // npm install eslint eslint-config-standard eslint-plugin-standard eslint-plugin-import eslint-plugin-n eslint-plugin-promise --save-dev
+}
